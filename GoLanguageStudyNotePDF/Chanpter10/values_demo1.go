e8791edde45b43b8247fcee185f4f30aa1343c3b
madwenoma <madwenoma@gmail.com>
2018-11-06 20:43:15 +0800

    reflect demo

--- GoLanguageStudyNotePDF/Chanpter10/values_demo1.go
+++ GoLanguageStudyNotePDF/Chanpter10/values_demo1.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"unsafe"
 )
+
 //P208
 
 type User struct {
@@ -34,5 +35,30 @@ func main() {
 	}
 
 	fmt.Printf("%+v\n", *u)
+
+	fmt.Println("=============通过反射改变struct属性值=============")
+	type user struct {
+		Name string
+		Age  int
+	}
+	user1 := user{
+		"q.yuhen",
+		34,
+	}
+
+	tu := reflect.ValueOf(&user1)//取地址值
+
+	if !tu.CanInterface() {
+		fmt.Println("CanInterface:fail1.")
+		return
+	}
+
+	if pu, ok := tu.Interface().(*user); ok {//tu.Interface().(*user);
+		pu.Age++
+		fmt.Printf("%+v\n", user1)
+	} else {
+		fmt.Println("CanInterface:fail.")
+	}
+
 
 }
